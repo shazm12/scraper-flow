f55df71932cd32df8c46233ebf10950c1275495a
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-04-23 22:18:25 +0530

    PROJECT INIT

---
+++ components/BreadcrumbHeader.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+import React from 'react'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbLink, BreadcrumbSeparator } from './ui/breadcrumb';
+
+function BreadcrumbHeader() {
+    const pathName = usePathname();
+    const paths = pathName === "/" ? [""]: pathName.split("/");
+    return (
+        <div className="flex item-centers flex-start">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    {paths.map((path,index) => (
+                        <React.Fragment key={index}>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink className="capitalize" href={`/${path}`}>
+                                    {path === "" ? "home": path}
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                        </React.Fragment>
+                    ))}
+                </BreadcrumbList>
+            </Breadcrumb>
+        </div>
+    )
+}
+
+export default BreadcrumbHeader;
