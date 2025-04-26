91bc146a347bd451ea4bad8b334d2f2415f99a5a
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-04-26 17:50:57 +0530

    chore: enable Theme Provider;

---
+++ components/providers/AppProviders.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+export function AppProviders({children}: {children: React.ReactNode}) {
+
+    return <ThemeProvider attribute="class"
+        defaultTheme="system" enableSystem>
+           {children} 
+        </ThemeProvider>
+
+} 
