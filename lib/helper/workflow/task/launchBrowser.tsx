e6ba387e2fe76a87c792640b49ecaa01f506f27b
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-03 15:42:36 +0530

    chore: added helper funtions to track the created node and groupings;

---
+++ lib/helper/workflow/task/launchBrowser.tsx
@@ -0,0 +1,10 @@
+import { TaskType } from "@/types/task";
+import { GlobeIcon, LucideProps } from "lucide-react";
+
+
+export const LaunchBrowserTask = {
+    type: TaskType.LAUNCH_BROWSER,
+    label: "Launch Browser",
+    icon: (props: LucideProps) => ( <GlobeIcon className="stoke-pink-400" {...props} /> ),
+    isEntryPoint: true
+};
---
+++ lib/helper/workflow/task/registry.tsx
@@ -0,0 +1,6 @@
+import { LaunchBrowserTask } from "./launchBrowser";
+
+export const TaskRegistry = {
+    LAUNCH_BROWSER: LaunchBrowserTask,
+};
+
