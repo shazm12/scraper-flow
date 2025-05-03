977d61f930184f2390b9466811450d5ec7e45d85
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-03 17:23:36 +0530

    chore: set view and func configs for the editor screen;

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -11,7 +11,10 @@ import NodeComponent from './nodes/nodeComponent';
 
 const nodeTypes = {
     FlowScrapeNode: NodeComponent,
-}
+};
+const snapGrid: [number, number] = [50, 50];
+
+const fitViewOptions = { padding : 1 };
 
 function FlowEditor({ workflow }: { workflow: Workflow}) {
     const [nodes, setNodes, onNodeChange] = useNodesState([
@@ -21,8 +24,8 @@ function FlowEditor({ workflow }: { workflow: Workflow}) {
 
     return (
     <main className="h-full w-full">
-      <ReactFlow nodes={nodes} edges={edges} onEdgesChange={onEdgeChange} onNodesChange={onNodeChange} nodeTypes={nodeTypes}>
-        <Controls position="top-left" />
+      <ReactFlow nodes={nodes} edges={edges} onEdgesChange={onEdgeChange} onNodesChange={onNodeChange} nodeTypes={nodeTypes} snapToGrid snapGrid={snapGrid} fitView fitViewOptions={fitViewOptions}>
+        <Controls position="top-left" fitViewOptions={fitViewOptions}/>
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
       </ReactFlow>
     </main>
