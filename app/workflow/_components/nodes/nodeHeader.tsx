adf824e3ada8a73c7595c72b437158e8d42c8a74
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-09 20:18:58 +0530

    chore: add logic to delete and dup nodes;

--- app/workflow/_components/nodes/nodeHeader.tsx
+++ app/workflow/_components/nodes/nodeHeader.tsx
@@ -1,31 +1,76 @@
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
-import { TaskRegistry } from '@/lib/helper/workflow/task/registry';
-import { TaskType } from '@/types/task';
-import { CoinsIcon, GripVerticalIcon } from 'lucide-react';
-import React from 'react'
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { CreateFlowNode } from "@/lib/helper/workflow/createFlowNode";
+import { TaskRegistry } from "@/lib/helper/workflow/task/registry";
+import { AppNode } from "@/types/appNode";
+import { TaskType } from "@/types/task";
+import { useReactFlow } from "@xyflow/react";
+import { CoinsIcon, CopyIcon, GripVerticalIcon, TrashIcon } from "lucide-react";
+import React from "react";
 
-function NodeHeader({ taskType }: { taskType: TaskType }) {
+function NodeHeader({
+  taskType,
+  nodeId,
+}: {
+  taskType: TaskType;
+  nodeId: string;
+}) {
   const task = TaskRegistry[taskType];
-    return (
+
+  const { deleteElements, getNode, addNodes } = useReactFlow();
+
+  return (
     <div className="flex items-center justify-around gap-2 p-2">
-        <task.icon size={16} />
-        <p className="flex items-center self-center justify-self-center text-xs font-bold uppercase text-muted-foreground w-1/4">
-            {task.label}
-        </p>
-        <div className="flex gap-1 items-center">
-            {task.isEntryPoint && <Badge>Entry Point</Badge>}
-            <Badge className="gap-2 flex items-center text-xs">
-                <CoinsIcon size={16} />
-                TODO
-            </Badge>
-            <Button variant={"ghost"} size={"icon"} className="drag-handle cursor-grab">
-                <GripVerticalIcon />
+      <task.icon size={16} />
+      <p className="flex items-center self-center justify-self-center text-xs font-bold uppercase text-muted-foreground w-1/4">
+        {task.label}
+      </p>
+      <div className="flex gap-1 items-center">
+        {task.isEntryPoint && <Badge>Entry Point</Badge>}
+        <Badge className="gap-2 flex items-center text-xs">
+          <CoinsIcon size={16} />
+          TODO
+        </Badge>
+        {!task.isEntryPoint && (
+          <>
+            <Button
+              variant={"ghost"}
+              size={"icon"}
+              onClick={() => {
+                deleteElements({
+                  nodes: [{ id: nodeId }],
+                });
+              }}
+            >
+              <TrashIcon size={12} />
             </Button>
-        </div>
-      
+            <Button variant={"ghost"} size={"icon"} onClick={() => {
+                const node = getNode(nodeId) as AppNode;
+                const newX = node.position.x ;
+                const newY = node.position.y;
+
+                const newNode = CreateFlowNode(node.data.type, {
+                    x: newX,
+                    y: newY
+                });
+
+                addNodes([newNode]);
+
+            }}>
+              <CopyIcon size={12} />
+            </Button>
+          </>
+        )}
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          className="drag-handle cursor-grab"
+        >
+          <GripVerticalIcon />
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default NodeHeader;
