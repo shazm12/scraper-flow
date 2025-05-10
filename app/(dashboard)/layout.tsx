0490890df9d43651ecb0fa3d0bedf914b23b9bba
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-10 17:41:09 +0530

    chore: style topbar of the app

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ function layout({children}: {children: React.ReactNode}) {
     <div className="flex h-screen">
         <DesktopSidebar />
         <div className="flex flex-col flex-1 min-h-screen">
-            <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
+            <header className="flex items-center justify-between px-6 py-5 h-[50px] container">
                 <BreadcrumbHeader />
                 <div className="gap-1 flex items-center">
                   <ModeToggle />
