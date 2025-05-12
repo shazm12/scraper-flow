d5a741060d49e50a37b23a122fc0967646e3fc8f
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-13 00:51:54 +0530

    chore: show node id in dev mode;

--- app/workflow/_components/nodes/nodeComponent.tsx
+++ app/workflow/_components/nodes/nodeComponent.tsx
@@ -6,7 +6,9 @@ import { AppNodeData } from "@/types/appNode";
 import { TaskRegistry } from "@/lib/helper/workflow/task/registry";
 import NodeInputs, { NodeInput } from "./nodeInputs";
 import { NodeOutput, NodeOutputs } from "./nodeOutputs";
+import { Badge } from "@/components/ui/badge";
 
+const DEV_MODE = process.env.NEXT_PUBLIC_DEV_MODE === "true";
 
 const NodeComponent = memo((props: NodeProps) => {
     const nodeData = props.data as AppNodeData;
@@ -14,6 +16,7 @@ const NodeComponent = memo((props: NodeProps) => {
     const task = TaskRegistry[nodeData.type];
 
     return <NodeCard nodeId={props.id} isSelected={props.selected}>
+        {DEV_MODE && <Badge>DEV:{props.id}</Badge>}
        <NodeHeader taskType={nodeData.type} nodeId={props.id} />
        <NodeInputs>
             {task.inputs.map((input, idx) => (
