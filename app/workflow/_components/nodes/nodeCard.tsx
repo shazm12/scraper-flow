b55e6ae005f84e1328f3ec19406b4731d0d1f9bd
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-13 22:54:32 +0530

    chore: display error message and  the error nodes and their inputs with errors;

--- app/workflow/_components/nodes/nodeCard.tsx
+++ app/workflow/_components/nodes/nodeCard.tsx
@@ -1,31 +1,48 @@
-import { cn } from '@/lib/utils';
-import { useReactFlow } from '@xyflow/react';
-import React, { ReactNode } from 'react'
+import useFlowValidation from "@/components/hooks/useFlowValidation";
+import { cn } from "@/lib/utils";
+import { useReactFlow } from "@xyflow/react";
+import React, { ReactNode } from "react";
 
-function NodeCard({children , nodeId, isSelected} : { children: ReactNode, nodeId: string, isSelected: boolean }) {
+function NodeCard({
+  children,
+  nodeId,
+  isSelected,
+}: {
+  children: ReactNode;
+  nodeId: string;
+  isSelected: boolean;
+}) {
   const { getNode, setCenter } = useReactFlow();
+  const { invalidInputs } = useFlowValidation();
+
+  const hasInvalidInput = invalidInputs.some((node) => node.nodeId === nodeId);
+
   return (
-    <div 
+    <div
       onDoubleClick={() => {
         const node = getNode(nodeId);
-        if(!node) return;
+        if (!node) return;
         const { position, measured } = node;
 
-        if(!position || !measured) return;
-        
-        const { width , height } = measured;
-        const x = position.x + width!/2;
-        const y =position.y  + height!/2;
+        if (!position || !measured) return;
 
-        if(x == undefined  || y == undefined ) return;
+        const { width, height } = measured;
+        const x = position.x + width! / 2;
+        const y = position.y + height! / 2;
 
-        setCenter(x,y, { zoom : 1, duration: 500});
+        if (x == undefined || y == undefined) return;
 
+        setCenter(x, y, { zoom: 1, duration: 500 });
       }}
-    className={cn("rounded-md cursor pointer bg-background border-2 border-separate w-[420px] text-xs flex flex-col", isSelected && "border-primary")}>
+      className={cn(
+        "rounded-md cursor pointer bg-background border-2 border-separate w-[420px] text-xs flex flex-col",
+        isSelected && "border-primary",
+        hasInvalidInput && "border-destructive border-2"
+      )}
+    >
       {children}
     </div>
-  )
+  );
 }
 
 export default NodeCard;
--- app/workflow/_components/nodes/nodeInputs.tsx
+++ app/workflow/_components/nodes/nodeInputs.tsx
@@ -4,6 +4,7 @@ import { Handle, Position, useEdges } from "@xyflow/react";
 import React, { ReactNode } from "react";
 import NodeParamField from "./nodeParamField";
 import { colorForHandles } from "./common";
+import useFlowValidation from "@/components/hooks/useFlowValidation";
 
 function NodeInputs({ children }: { children: ReactNode }) {
   return <div className="flex flex-col divide-y gap-2">{children}</div>;
@@ -16,14 +17,24 @@ export function NodeInput({
   input: TaskParam;
   nodeId: string;
 }) {
+  const { invalidInputs } = useFlowValidation();
   const edges = useEdges();
 
   const isConnected = edges.some(
     (edge) => edge.target === nodeId && edge.targetHandle === input.name
   );
 
+  const hasErrors = invalidInputs
+    .find((node) => node.nodeId === nodeId)
+    ?.inputs.find((invalidInput) => invalidInput === input.name);
+
   return (
-    <div className="flex justify-start relative p-3 bg-secondary w-full">
+    <div
+      className={cn(
+        "flex justify-start relative p-3 bg-secondary w-full",
+        hasErrors && "bg-destructive/30"
+      )}
+    >
       <NodeParamField param={input} nodeId={nodeId} disabled={isConnected} />
       {!input.hideHandle && (
         <Handle
