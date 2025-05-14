071473e110fe21e3eae8d3e82912843a5c2319d8
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-14 23:16:29 +0530

    chore: create server action t o create workflow Execution Plan in db;

---
+++ actions/workflows/runWorkFlow.ts
@@ -0,0 +1,84 @@
+"use server";
+
+import { FlowToExecutionPlan } from "@/lib/helper/workflow/executionPlan";
+import { TaskRegistry } from "@/lib/helper/workflow/task/registry";
+import { prisma } from "@/lib/prisma";
+import { WorkflowExecutionPlan } from "@/types/workflow";
+import { auth } from "@clerk/nextjs/server";
+import { number } from "zod";
+
+
+
+export async function RunWorkFlow(form: {
+    workflowId: string;
+    flowDefination?: string;
+}) {
+    const { userId } = auth();
+    if(!userId) {
+        throw new Error("Unauthenticated");
+    }
+
+    const { workflowId, flowDefination } = form;
+    if(!workflowId) {
+        throw new Error("workflowId is requied");
+    }
+
+    const workflow = await prisma.workflow.findUnique({
+        where: {
+            userId,
+            id: workflowId
+        }
+    });
+
+    if(!workflow) {
+        throw new Error("Flow Defination is not defined!");
+    }
+
+    let executionPlan: WorkflowExecutionPlan;
+    if(!flowDefination) {
+        throw new Error("Workflow Defination is undefined");
+    }
+
+    const flow = JSON.parse(flowDefination);
+    const result = FlowToExecutionPlan(flow.nodes, flow.edges);
+    
+    if(!result.executionPlan) {
+        throw new Error("No execution plan generated");
+    }
+
+    executionPlan = result.executionPlan;
+
+    const execution = await prisma.workflowExecution.create({
+        data: {
+            workflowId,
+            userId,
+            status: "PENDING",
+            startedAt: new Date(),
+            trigger: "manual",
+            phases: {
+                create: executionPlan.flatMap(phase => {
+                    return phase.nodes.flatMap((node) => {
+                        return {
+                            userId,
+                            status:"CREATED",
+                            number: phase.phase,
+                            node: JSON.stringify(node),
+                            name: TaskRegistry[node.data.type].label
+                        }
+                    })
+                })
+            }
+        },
+        select: {
+            id: true,
+            phases: true
+        },
+    });
+
+    if(!execution) {
+        throw new Error("Workflow Execution could not be created");
+    }
+
+
+    
+}
