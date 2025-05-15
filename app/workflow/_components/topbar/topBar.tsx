a39a8ca0a3a5e2d162821aa24ccd9dea87b4f835
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-16 00:34:42 +0530

    chore: add a flag to hide the buttons as and when req;

--- app/workflow/_components/topbar/topBar.tsx
+++ app/workflow/_components/topbar/topBar.tsx
@@ -1,38 +1,47 @@
-import TooltipWrapper from '@/components/TooltipWrapper';
-import { Button } from '@/components/ui/button';
-import { ChevronLeftIcon } from 'lucide-react';
-import React from 'react'
-import SaveButton from './saveButton';
-import { useRouter } from 'next/navigation';
-import ExecuteButton from './executeButton';
+import TooltipWrapper from "@/components/TooltipWrapper";
+import { Button } from "@/components/ui/button";
+import { ChevronLeftIcon } from "lucide-react";
+import React from "react";
+import SaveButton from "./saveButton";
+import { useRouter } from "next/navigation";
+import ExecuteButton from "./executeButton";
 
 interface Props {
-    title: string;
-    subtitle?: string;
-    workflowId: string;
+  title: string;
+  subtitle?: string;
+  workflowId: string;
+  hideButtons?: boolean;
 }
 
-function TopBar({ title, subtitle, workflowId }: Props ) {
-    const router = useRouter();
+function TopBar({ title, subtitle, workflowId, hideButtons = false }: Props) {
+  const router = useRouter();
   return (
     <header className="flex p-2 border-b-2 border-separate justify-between w-full h-[60px] sticky top-0 bg-background z-10">
       <div className="flex gap-1 flex-1">
         <TooltipWrapper content="Back">
-            <Button variant={"ghost"} size={"icon"} onClick={() => router.back()}>
-                <ChevronLeftIcon size={20} />
-            </Button>
+          <Button variant={"ghost"} size={"icon"} onClick={() => router.back()}>
+            <ChevronLeftIcon size={20} />
+          </Button>
         </TooltipWrapper>
         <div>
-            <p className="font-bold text-ellipsis truncate">{title}</p>
-            { subtitle && (<p className="text-xs text-muted-foreground truncate text-ellipsis">{subtitle}</p>)}
+          <p className="font-bold text-ellipsis truncate">{title}</p>
+          {subtitle && (
+            <p className="text-xs text-muted-foreground truncate text-ellipsis">
+              {subtitle}
+            </p>
+          )}
         </div>
       </div>
       <div className="flex gap-1 flex-1 justify-end">
-        <ExecuteButton workflowId={workflowId} />
-        <SaveButton workflowId={workflowId} />
+        {!hideButtons && (
+          <>
+            <ExecuteButton workflowId={workflowId} />
+            <SaveButton workflowId={workflowId} />
+          </>
+        )}
       </div>
     </header>
-  )
+  );
 }
 
 export default TopBar;
