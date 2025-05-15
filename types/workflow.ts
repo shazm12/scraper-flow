6f2f9f215bffa3139e764485c4759fae5c5263f6
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-16 00:33:10 +0530

    chore: create enums for WorkflowExecutionStatus and ExecutionPhase and TriggerType;

--- types/workflow.ts
+++ types/workflow.ts
@@ -26,3 +26,31 @@ export type WorkflowExecutionPLanPhase = {
 
 
 export type WorkflowExecutionPlan = WorkflowExecutionPLanPhase[];
+
+export enum ExecutionStatus {
+    PENDING="PENDING",
+    RUNNING="RUNNING",
+    COMPLETED="COMPLETED",
+    FAILED="FAILED"
+}
+
+export enum WorkflowExecutionTrigger {
+    MANUAL="MANUAL"
+}
+
+export enum WorkflowExecutionStatus {
+    PENDING="PENDING",
+    RUNNING="RUNNING",
+    COMPLETED="COMPLETED",
+    FAILED="FAILED"
+}
+
+export enum ExecutionPhaseStatus {
+    CREATED="CREATED",
+    PENDING="PENDING",
+    RUNNING="RUNNING",
+    COMPLETED="COMPLETED",
+    FAILED="FAILED"
+}
+
+
