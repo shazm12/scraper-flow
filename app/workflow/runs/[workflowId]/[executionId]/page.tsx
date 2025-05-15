ab19b4ae94df5489652f88c7bea6f15444abf593
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-16 00:33:39 +0530

    feat(minor): added new page WorkflowExecutionViewer;

---
+++ app/workflow/runs/[workflowId]/[executionId]/page.tsx
@@ -0,0 +1,58 @@
+import { getWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowExecutionWithPhases";
+import TopBar from "@/app/workflow/_components/topbar/topBar";
+import { auth } from "@clerk/nextjs/server";
+import { Loader2Icon } from "lucide-react";
+import React, { Suspense } from "react";
+
+function ExecutionViewerPage({
+  params,
+}: {
+  params: { workflowId: string; executionId: string };
+}) {
+  return (
+    <div className="flex flex-col h-screen w-full overflow-hidden">
+      <TopBar
+        workflowId={params.workflowId}
+        title="Workflow Run Details"
+        subtitle={`Run ID: ${params.executionId}`}
+        hideButtons
+      />
+      <section className="flex h-full overflow-auto">
+        <Suspense fallback={
+          <div className="flex w-full justify-center items-center">
+            <Loader2Icon className="h-10 w-10 animate-spin stroke-primary" />
+          </div>
+        }>
+          <ExectionViewerWrapper executionId={params.executionId} />
+        </Suspense>
+      </section>
+    </div>
+  );
+}
+
+async function ExectionViewerWrapper({
+  executionId,
+}: {
+  executionId: string;
+}) {
+
+  const {userId} = auth();
+  if(!userId) {
+    return <div>Unauthenticated</div>;
+  }
+
+
+  const workflowExecution = await getWorkflowExecutionWithPhases(executionId);
+  if(!workflowExecution) {
+    return <div>Not Found</div>;
+  }
+
+
+  return (
+    <pre>
+      {JSON.stringify(workflowExecution, null, 4)}
+    </pre>
+  );
+}
+
+export default ExecutionViewerPage;
