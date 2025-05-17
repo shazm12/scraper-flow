62167f58ee53bf0f49ac436865c20f41bf75187b
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-17 12:01:12 +0530

    chore: added helper func to calculate duration;

--- app/workflow/runs/[workflowId]/[executionId]/_components/executionViewer.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/_components/executionViewer.tsx
@@ -3,10 +3,19 @@ import { getWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowE
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { DatesToDurationString } from "@/lib/helper/dates";
 import { WorkflowExecutionStatus } from "@/types/workflow";
 import { useQuery } from "@tanstack/react-query";
 import { formatDistanceToNow } from "date-fns";
-import { CalendarIcon, CircleDashedIcon, ClockIcon, CoinsIcon, LucideIcon, WorkflowIcon } from "lucide-react";
+import {
+  CalendarIcon,
+  CircleDashedIcon,
+  ClockIcon,
+  CoinsIcon,
+  Loader2Icon,
+  LucideIcon,
+  WorkflowIcon,
+} from "lucide-react";
 import React, { ReactNode } from "react";
 
 type ExecutionData = Awaited<ReturnType<typeof getWorkflowExecutionWithPhases>>;
@@ -19,6 +28,12 @@ function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
     refetchInterval: (q) =>
       q.state.data?.status === WorkflowExecutionStatus.RUNNING ? 1000 : false,
   });
+
+  const duration = DatesToDurationString(
+    query?.data?.completedAt,
+    query?.data?.startedAt
+  );
+
   return (
     <div className="flex w-full h-full">
       <aside className="w-[440px] min-w-[440px] max-w-[440px] border-r-2 border-separate flex flex-grow flex-col overflow-hidden">
@@ -41,11 +56,7 @@ function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
               </span>
             }
           />
-          <ExecutionLabel
-            Icon={ClockIcon}
-            label="Duration"
-            value={"TODO"}
-          />
+          <ExecutionLabel Icon={ClockIcon} label="Duration" value={duration ? duration: <Loader2Icon className="animate-spin" size={20} />} />
           <ExecutionLabel
             Icon={CoinsIcon}
             label="Credits Consumed"
@@ -54,25 +65,25 @@ function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
         </div>
         <Separator />
         <div className="flex justify-center items-center py-2 px-4">
-            <div className="text-muted-foreground flex items-center gap-2">
-                <WorkflowIcon size={20} className="stroke-muted-foreground" />
-                <span className="font-semibold">
-                    Phases
-                </span>
-            </div>
+          <div className="text-muted-foreground flex items-center gap-2">
+            <WorkflowIcon size={20} className="stroke-muted-foreground" />
+            <span className="font-semibold">Phases</span>
+          </div>
         </div>
         <Separator />
         <div className="overflow-auto h-full px-2 py-4">
-            {query?.data?.phases.map((phase,index) => (
-                <Button key={phase.id} variant={"ghost"} className="w-full justify-between">
-                  <div className="flex items-center gap -2">
-                      <p className="font-semibold">{phase.name}</p>
-                      <Badge variant={"outline"}>
-                        {index + 1 }
-                      </Badge>
-                  </div>
-                </Button>
-            ))}
+          {query?.data?.phases.map((phase, index) => (
+            <Button
+              key={phase.id}
+              variant={"ghost"}
+              className="w-full justify-between"
+            >
+              <div className="flex items-center gap -2">
+                <p className="font-semibold">{phase.name}</p>
+                <Badge variant={"outline"}>{index + 1}</Badge>
+              </div>
+            </Button>
+          ))}
         </div>
       </aside>
     </div>
