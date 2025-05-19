6527734e662eed0b3696d6318cd154b325cd4964
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-19 21:20:24 +0530

    chore: added new types for Execution Environment;

---
+++ types/executor.ts
@@ -0,0 +1,22 @@
+import { Browser, Page } from "puppeteer"
+import { WorkflowTask } from "./workflow"
+
+export type Environment = {
+    browser?: Browser,
+    page?: Page,
+    phases: {
+        // node id as keys are used
+        [key: string] : {
+            inputs: Record<string,string>,
+            outputs: Record<string,string>,
+        }
+    }
+}
+
+export type ExecutionEnvironment<T extends WorkflowTask> = {
+    getInput(name: T["inputs"][number]["name"]): string
+    getBrowser(): Browser | undefined;
+    setBrowser(browser: Browser) : void; 
+    getPage(): Page | undefined;
+    setPage(page: Page): void;
+}
