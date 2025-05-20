6d2578435bffd29eecf8628c7198dcec02017a43
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-20 23:26:55 +0530

    feat(minor): define the extractTextFromHtml module;

--- lib/helper/workflow/executor/extractTextFromHTMLExec.ts
+++ lib/helper/workflow/executor/extractTextFromHTMLExec.ts
@@ -1,10 +1,40 @@
 import { ExecutionEnvironment } from "@/types/executor";
 import { waitFor } from "../../waitFor";
+import { ExtractTextFromHtmlTask } from "../task/extractTextFromHtml";
+import * as cheerio from "cheerio";
 
 export async function extractTextFromHTMLExecutor(
-  environment: ExecutionEnvironment<any>
+  environment: ExecutionEnvironment<typeof ExtractTextFromHtmlTask>
 ): Promise<boolean> {
-  const websiteUrl = environment.getInput("Website URL");
+  const selector = environment.getInput("selector");
+  if(!selector) {
+    return false;
+  }
+
+  const html = environment.getInput("Html");
+
+  const $ = cheerio.load(html);
+  const element = $(selector);
+
+  if(!element) {
+    console.error("Element not found");
+    return false;
+  }
+
+  const extractedText = $.text(element);
+  if(!extractedText) {
+    console.error("Element has no text");
+    return false;
+  }
+
+  environment.setOutput("Extracted Text",extractedText);
+
+
+  if(!html) {
+    return false;
+  }
+
+
 
   try {
     await waitFor(3000);
