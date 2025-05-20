1e59ba7e34357ffdbdfce3a54b107f6014a54682
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-05-20 23:27:42 +0530

    chore: set environment vars;

--- types/executor.ts
+++ types/executor.ts
@@ -15,8 +15,12 @@ export type Environment = {
 
 export type ExecutionEnvironment<T extends WorkflowTask> = {
     getInput(name: T["inputs"][number]["name"]): string
+    setOutput(name:T["outputs"][number]["name"], value: string) :void;
     getBrowser(): Browser | undefined;
     setBrowser(browser: Browser) : void; 
     getPage(): Page | undefined;
     setPage(page: Page): void;
 }
+
+
+
